89aa34245ff1b670635bf8260fc2d113f30cc871
James Dorrington <jdorrington@hotmail.com>
2020-06-05 08:46:00 +0100

    Colours basically working

--- src/Lambda/src/Lambda/Main.java
+++ src/Lambda/src/Lambda/Main.java
@@ -7,14 +7,10 @@ public class Main {
         for (int i = 0; i< args.length; i++){
             System.out.println(args[i]);
         }
-        
-        Console.println("u039B = \u039B");
-        Console.println("u03BB = \u03BB");
+                
+        Console.println("u039B = \u039B", Console.Color.RED);
+        Console.println("u03BB = \u03BB", Console.Color.RED, Console.Color.YELLOW);
         Console.println("u1D27 = \u1D27");
-        
-        System.out.print("\u001B[0;33m");
-        System.out.println("YELLOW");
-        System.out.print("\033[0m");
 
         
         String[] tokens = Tokeniser.Tokenise("true = λx.λy.x");
