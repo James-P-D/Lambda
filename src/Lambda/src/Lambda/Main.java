2ead7a4384bb565f85d07b22e91263696bc26171
James Dorrington <jdorrington@hotmail.com>
2020-06-05 17:04:52 +0100

    More formatting

--- src/Lambda/src/Lambda/Main.java
+++ src/Lambda/src/Lambda/Main.java
@@ -9,55 +9,19 @@ import java.io.InputStreamReader;
 
 
 public class Main {
-
-    private static String LAMBDA_CHAR = "\u03BB"; 
-    private static String ALPHA_CHAR = "\u03B1"; 
-    private static String BETA_CHAR = "\u03B2";
+    // Greek letters                               // Upper  Lower   
+    private static String ALPHA_CHAR   = "\u03B1"; // \u0391 \u03B1 
+    private static String BETA_CHAR    = "\u03B2"; // \u0392 \u03B2
+    private static String ETA_CHAR     = "\u03B7"; // \u0397 \u03B7
+    private static String LAMBDA_CHAR  = "\u03BB"; // \u039B \u03BB    
     
     public static void main(String[] args) {
         parseArguments(args);
         
-        /*        
-        Console.println("u039B = \u039B", Console.Color.GREEN);
-        Console.println("u03BB = \u03BB", Console.Color.RED, Console.Color.YELLOW_BACKGROUND);
-        Console.println("u1D27 = \u1D27");
-        Console.println("");
-
-        Console.print("X", Console.Color.BLACK_BOLD);
-        Console.print("X", Console.Color.WHITE);
-        Console.print("X", Console.Color.WHITE_BOLD);
-        Console.println("");
-
-        Console.print(LAMBDA_CHAR + "> ", Console.Color.BLACK_BOLD);
-        Console.println("");
-        
-        Console.print(ALPHA_CHAR + "> ", Console.Color.BLACK_BOLD);
-        Console.println("");
-
-        Console.print(BETA_CHAR + "> ", Console.Color.BLACK_BOLD);
-        Console.println("");
-        Console.println("");
-             
-        Console.print(LAMBDA_CHAR + "> ", Console.Color.BLACK_BOLD);
-        String[] tokens = Tokeniser.Tokenise("mult = λm.λn.λf.m (n f)");
-        for (int i = 0; i < tokens.length; i++) {
-            String token = tokens[i];
-            if (token.equals(LAMBDA_CHAR)) {
-                Console.print(token, Console.Color.YELLOW_BOLD);                        
-            } else if ((token.equals("(")) ||
-                       (token.equals(")")) ||
-                       (token.equals(".")) ||
-                       (token.equals("="))) {
-                Console.print(token, Console.Color.GREEN);                        
-            } else {
-                Console.print(token, Console.Color.WHITE_BOLD);              
-            }
-        }
-        Console.println("");
-        Console.println("");
-        
-        displayError("There was a problem!");
-        */
+        Console.print(ALPHA_CHAR + "> ", Console.Color.BLACK_BOLD); Console.println("");
+        Console.print(BETA_CHAR + "> ", Console.Color.BLACK_BOLD); Console.println("");
+        Console.print(ETA_CHAR + "> ", Console.Color.BLACK_BOLD); Console.println("");
+        Console.print(LAMBDA_CHAR + "> ", Console.Color.BLACK_BOLD); Console.println("");
     }
 
     private static void parseAndOutput(String input){
