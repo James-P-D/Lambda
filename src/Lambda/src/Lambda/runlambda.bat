fdde069d51884286d93447a3872d4263c4fc7afc
James Dorrington <jdorrington@hotmail.com>
2020-06-08 23:01:44 +0100

    Updating build script

--- src/Lambda/src/Lambda/runlambda.bat
+++ src/Lambda/src/Lambda/runlambda.bat
@@ -1,5 +1,5 @@
 @echo off
-del *.class
+del *.class > nul
 
 rem ORIGINAL FROM ECLIPSE:
 rem "C:\Program Files\Java\jre1.8.0_251\bin\javaw.exe" -agentlib:jdwp=transport=dt_socket,suspend=y,address=localhost:21341 -Dfile.encoding=UTF-8 -classpath "C:\Users\jdorr\Desktop\Dev\Lambda\src\Lambda\bin;C:\Users\jdorr\Desktop\Dev\Lambda\src\Lambda\libs\jna-platform-5.5.0.jar;C:\Users\jdorr\Desktop\Dev\Lambda\src\Lambda\libs\jna-5.5.0.jar" Lambda.Main
@@ -14,5 +14,5 @@ rem javac -cp ..\..\libs\jna-5.5.0.jar;..\..\libs\jna-platform-5.5.0.jar;. Conso
 rem javac -encoding utf8 Tokeniser.java
 rem javac -cp ;. -encoding utf8 Main.java
 
-javac -cp ..\..\libs\jna-5.5.0.jar;..\..\libs\jna-platform-5.5.0.jar;. Constants.java Console.java Tokeniser.java RawConsoleInput.java -encoding utf8 Main.java
-java -classpath "C:\Users\jdorr\Desktop\Dev\Lambda\src\Lambda\bin;..\..\libs\jna-5.5.0.jar;..\..\libs\jna-platform-5.5.0.jar" Lambda.Main C:\Users\jdorr\Desktop\Dev\Lambda\src\Lambda\src\Lambda\definitions.lbd
+javac -cp ..\..\libs\jna-5.5.0.jar;..\..\libs\jna-platform-5.5.0.jar;. Constants.java Console.java Tokeniser.java RawConsoleInput.java LambdaExpression.java LambdaName.java LambdaFunction.java LambdaApplication.java -encoding utf8 Main.java
+java -Xss4m -classpath "C:\Users\jdorr\Desktop\Dev\Lambda\src\Lambda\bin;..\..\libs\jna-5.5.0.jar;..\..\libs\jna-platform-5.5.0.jar" Lambda.Main C:\Users\jdorr\Desktop\Dev\Lambda\src\Lambda\src\Lambda\definitions.lbd
