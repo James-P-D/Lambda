607c3c4a96c6b44cf00222860b0276ec41e36b94
James Dorrington <jdorrington@hotmail.com>
2020-06-20 23:13:15 +0100

    Minor change to build script

--- src/Lambda/src/Lambda/runlambda.bat
+++ src/Lambda/src/Lambda/runlambda.bat
@@ -1,4 +1,4 @@
-@echo off
+rem @echo off
 del *.class > nul
 
 javac -cp ..\..\libs\jna-5.5.0.jar;..\..\libs\jna-platform-5.5.0.jar;. Constants.java Console.java Tokeniser.java Parser.java DisplayMessage.java RawConsoleInput.java LambdaExpression.java LambdaName.java LambdaFunction.java LambdaApplication.java ParseException.java IntRef.java Main.java
