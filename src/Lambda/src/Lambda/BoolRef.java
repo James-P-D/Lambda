0bf55ced9698a68454e0b46bb2721cd647c40513
James Dorrington <jdorrington@hotmail.com>
2020-08-26 18:40:41 +0100

    updates

---
+++ src/Lambda/src/Lambda/BoolRef.java
@@ -0,0 +1,10 @@
+package Lambda;
+
+// Wrapper for 'boolean'.
+public class BoolRef {
+    public boolean value;
+    
+    public BoolRef(boolean i) {
+        this.value = i;
+    }
+}
--- src/Lambda/src/Lambda/IntRef.java
+++ src/Lambda/src/Lambda/IntRef.java
@@ -1,7 +1,7 @@
 package Lambda;
 
 // Wrapper for 'int'.
-// Because 'int' is a primative type, it is always passed by value, so we cannot expect
+// Because 'int' is a primitive type, it is always passed by value, so we cannot expect
 // changes to its value to be retained after returning from a function. Since we'll be
 // using an 'index' variable to keep track of the token we are processing when we parse
 // a lambda-string, we'll need to use this class.
